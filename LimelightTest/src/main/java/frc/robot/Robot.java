da9587fe14d318d2ce40c1e30f879e5852497c2f
David Garcia <garciadavid2003@gmail.com>
2020-01-22 15:46:44 -0500

    bruh

--- LimelightTest/src/main/java/frc/robot/Robot.java
+++ LimelightTest/src/main/java/frc/robot/Robot.java
@@ -130,12 +130,13 @@ public class Robot extends TimedRobot {
         final double STEER_K = 0.03;                    // how hard to turn toward the target
         final double DRIVE_K = 0.26;                    // how hard to drive fwd toward the target
         final double DESIRED_TARGET_AREA = 13.0;        // Area of the target when the robot reaches the wall
-        final double MAX_DRIVE = 0.7;                   // Simple speed limit so we don't drive too fast
+        final double MAX_DRIVE = 0.5;                   // Simple speed limit so we don't drive too fast
 
         double tv = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tv").getDouble(0);
         double tx = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0);
         double ty = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ty").getDouble(0);
         double ta = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ta").getDouble(0);
+        double camMode = NetworkTableInstance.getDefault().getTable("limelight").getEntry("camMode").setDouble(0);
 
         if (tv < 1.0)
         {
