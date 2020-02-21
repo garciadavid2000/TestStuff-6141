80573149c34e82f589a9aceebf3a649a2d3e0820
David Garcia <garciadavid2003@gmail.com>
2020-02-20 22:29:46 -0500

    testing

--- Encoder/src/main/java/frc/robot/Robot.java
+++ Encoder/src/main/java/frc/robot/Robot.java
@@ -7,13 +7,14 @@
 
 package frc.robot;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
-
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.PWMVictorSPX;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.DriverStation;
+
 
 /**
  * This sample program shows how to control a motor using a joystick. In the
@@ -22,23 +23,46 @@ import edu.wpi.first.wpilibj.VictorSP;
  *
  * <p>Joystick analog values range from -1 to 1 and speed controller inputs also
  * range from -1 to 1 making it easy to work together.
+ *
+ * <p>In addition, the encoder value of an encoder connected to ports 0 and 1 is
+ * consistently sent to the Dashboard.
  */
 public class Robot extends TimedRobot {
-  private static final int kMotorPort = 5;
+  private static final int kMotorPort = 0;
   private static final int kJoystickPort = 0;
+  private static final int kEncoderPortA = 0;
+  private static final int kEncoderPortB = 1;
 
-  
-  private WPI_VictorSPX motor = new WPI_VictorSPX(3);
+  private SpeedController m_motor;
+  private Joystick m_joystick;
+  private Encoder m_encoder;
+
 
   @Override
   public void robotInit() {
+    m_motor = new PWMVictorSPX(kMotorPort);
+    m_joystick = new Joystick(kJoystickPort);
+    m_encoder = new Encoder(kEncoderPortA, kEncoderPortB);
+    // Use SetDistancePerPulse to set the multiplier for GetDistance
+    // This is set up assuming a 6 inch wheel with a 360 CPR encoder.
+    m_encoder.setDistancePerPulse((Math.PI * 6) / 4096);
+
     
     
+
   }
-  
+
+  /*
+   * The RobotPeriodic function is called every control packet no matter the
+   * robot mode.
+   */
+  @Override
+  public void robotPeriodic() {
+    SmartDashboard.putNumber("Encoder", m_encoder.getDistance());
+  }
 
   @Override
   public void teleopPeriodic() {
-    motor.set(1);
+    m_motor.set(m_joystick.getY());
   }
 }
